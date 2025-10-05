047c19993ee93f708f1ba594c502caeb2131e1a6
pragyan <ghimirepragyan100@gmail.com>
2025-10-05 18:58:28 +0545

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+
+import middleware from "next-auth/middleware";
+
+export default middleware;
+
+export const config = {
+    // *: zero or more parameters
+    // +: one or more parameters
+    // ?: zero or one parameters
+    matcher: ['/dashboard/:path*','/bookmarks','/upload']
+    
+}
